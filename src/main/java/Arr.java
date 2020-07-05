b85c8b7091e3aa34a0e3945b052e925640705490
UstinKonst <ustinovskuk@mail.ru>
2020-07-06 01:55:32 +0300

    Initial commit

---
+++ src/main/java/Arr.java
@@ -0,0 +1,12 @@
+import com.sun.xml.internal.bind.v2.runtime.NamespaceContext2;
+
+public class Arr {
+    public static boolean arr2(int[] arr) {
+        for (int i = 0; i <arr.length; i++) {
+            if (arr[i] == 1 || arr[i] == 4) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ src/test/java/MassTestAddArr.java
@@ -0,0 +1,40 @@
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+
+import java.util.Arrays;
+import java.util.Collection;
+
+@RunWith(Parameterized.class)
+public class MassTestAddArr {
+    private int[] arr;
+    Arr arr1;
+
+
+    public MassTestAddArr(int[] arr) {
+        this.arr = arr;
+
+    }
+
+    @Parameterized.Parameters
+    public static Collection<Object[]> data() {
+        return Arrays.asList(new Object[][]{
+                {new int[]{1, 2, 6}},
+                {new int[]{2, 5, 8}},
+                {new int[]{3, 5, 4,}},
+        });
+    }
+
+    @Before
+    public void init() {
+        arr1 = new Arr();
+    }
+
+    @Test
+    public void testAdd() {
+        Assert.assertTrue(Arr.arr2(arr));
+    }
+
+}
